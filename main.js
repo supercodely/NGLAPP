569591e1b36ae1d9ba4a62de3b229db64d181407
supercodely <supercodely@gmail.com>
2026-08-19 06:42:03 +0200

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import { createElement } from "react";
-import { createRoot } from "react-dom/client";
-import TaskManager from "./app.js";
-
-const rootEl = document.getElementById("root");
-createRoot(rootEl).render(createElement(TaskManager));
